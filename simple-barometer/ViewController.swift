ccb5825c3d58c96bf2c8a53837fc45386d84f826
Michael Johnson <michaelthejohnson@gmail.com>
2023-02-09 14:44:12 -0500

    Initial Commit

---
+++ simple-barometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple-barometer
+//
+//  Created by Michael Johnson on 2/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
