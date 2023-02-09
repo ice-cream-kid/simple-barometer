c0c8f25206dba18962effb553917bcd89556da12
Michael Johnson <michaelthejohnson@gmail.com>
2023-02-09 15:30:57 -0500

    a bit of initial set up

---
+++ simple-barometer/StartPage.swift
@@ -0,0 +1,18 @@
+//
+//  StartPage.swift
+//  simple-barometer
+//
+//  Created by Michael Johnson on 2/9/23.
+//
+
+import Foundation
+import UIKit
+
+class StartPage : UIViewController {
+    
+    override func viewDidLoad() {
+        
+        self.view.backgroundColor = UIColor.systemMint;
+        
+    }
+}
