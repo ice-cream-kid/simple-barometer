242132a5fdc1db03441ca04a9356660f6c95a41c
Michael Johnson <michaelthejohnson@gmail.com>
2023-02-10 09:30:35 -0500

    All current outlets added, connected

--- simple-barometer/StartPage.swift
+++ simple-barometer/StartPage.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class StartPage : UIViewController {
     
+    @IBOutlet weak var titleStackViewContainer : UIView!
+    @IBOutlet weak var titleStackView : UIStackView!
+    @IBOutlet weak var titleLabel : UILabel!
+    @IBOutlet weak var subtitleLabel : UILabel!
     
+    @IBOutlet weak var startPagePanelsStackView : UIStackView!
+    
+    @IBOutlet weak var currentConditionsView : UIView!
+    @IBOutlet weak var currentConditionsLabel : UILabel!
+    @IBOutlet weak var currentConditionsButton : UIButton!
+
+    @IBOutlet weak var dayView : UIView!
+    @IBOutlet weak var dayViewLabel : UILabel!
+    @IBOutlet weak var dayViewButton : UIButton!
+    
+    @IBOutlet weak var tenDayView : UIView!
+    @IBOutlet weak var tenDayViewLabel : UILabel!
+    @IBOutlet weak var tenDayViewButton : UIButton!
+    
+    @IBOutlet weak var bottomInformationView : UIView!
+    @IBOutlet weak var bottomInformationLabel : UILabel!
+
     
     override func viewDidLoad() {
         
+        self.navigationController?.navigationBar.backgroundColor = UIColor.red
     }
 }
